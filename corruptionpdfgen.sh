6f6e099789e3a522da9e6b72bf3bc84338e24721
Ben Sharafian <benms64@gmail.com>
2015-03-18 17:52:31 -0700

    makes script more portable

--- corruptionpdfgen.sh
+++ corruptionpdfgen.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+ODIR=$( pwd ) # oh dear
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR
 
 if [ "$#" -eq 3 ]; then
-	cp $1 src
-	cp $2 src
+	cp $1 $DIR/src
+	cp $2 $DIR/src
 fi
 
-cd src
-./corruptionpdfgen.py $1 $2 "${3}" && mv "${3}.pdf" $DIR
+cd $DIR/src
+
+./corruptionpdfgen.py $1 $2 "${3}" && mv "${3}.pdf" $ODIR
 if [ "$#" -eq 3 ]; then
 	rm "${3}.aux" "${3}.log" "${3}.tex" "$1" "$2"
 fi
